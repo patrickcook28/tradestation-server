82752fd3564ba30e2ce15756d2fab57215a69c71
Patrick Cook <patrick.cook@procore.com>
2025-12-03 16:51:12 -0800

    small updates to market aggregates stream

--- utils/marketAggregatesStreamManager.js
+++ utils/marketAggregatesStreamManager.js
@@ -4,15 +4,17 @@ const mux = new StreamMultiplexer({
   name: 'MarketAggregates',
   makeKey: (userId, { ticker }) => [userId, ticker].join('|'),
   buildRequest: (userId, { ticker }) => ({
-    // Use Market Depth Quotes stream per TradeStation docs
-    // https://api.tradestation.com/docs/specification/#tag/MarketData/operation/StreamMarketDepthQuotes
-    path: `/marketdata/stream/marketdepth/quotes/${ticker}`,
+    // Use Market Depth Aggregates stream for aggregated bids/asks by price level
+    // https://api.tradestation.com/docs/specification/#tag/MarketData/operation/StreamMarketDepthAggregates
+    path: `/marketdata/stream/marketdepth/aggregates/${ticker}`,
     paperTrading: false,
     query: { maxlevels: '50' },
   })
 });
 
-module.exports = { ...mux, addSubscriber: mux.addExclusiveSubscriber.bind(mux) };
+// Use regular addSubscriber (not exclusive) to support multiple concurrent streams
+// for different tickers when user has multiple charts open
+module.exports = { ...mux, addSubscriber: mux.addSubscriber.bind(mux) };
 
 
 
